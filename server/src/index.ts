c3579b357f4280c7a7c24177c6f07faf5174bf2c
Максим <akaikinmax@mail.ru>
2026-02-19 19:52:10 +0700

    base server

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import type { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hihik");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
